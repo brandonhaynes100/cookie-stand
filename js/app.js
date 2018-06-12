3c8578bbbb1754c942f09f6e4889a725cd03f70a
Brandon Haynes <brandonhaynes100@gmail.com>
2018-06-12 16:57:43 -0700

    implementing table body display for multiple StoreLocation objects

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function StoreLocation(name, minCustPerHr, maxCustPerHr, avgCookiePerCust) {
   this.cookiesToday = 0;
 }
 
-// method prototypes for StorLocation objects
+// generates a random number of customers within the objects min/max range
 StoreLocation.prototype.randomCustPerHr = function() {
   // this formula for a random inclusive range was found here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   return Math.floor(Math.random() * (this.maxCustPerHr - this.minCustPerHr + 1)) + this.minCustPerHr; //The maximum is inclusive and the minimum is inclusive
@@ -45,7 +45,7 @@ StoreLocation.prototype.calculateDailyCookies = function() {
 };
 
 // renders the header row for the table
-StoreLocation.prototype.renderHeaderRow = function() {
+var renderHeaderRow = function() {
 
   // store table element in variable
   var tableEl = document.getElementById('cookieSalesTable');
@@ -68,19 +68,77 @@ StoreLocation.prototype.renderHeaderRow = function() {
     // th3 - append to the DOM
     headerRow.appendChild(headingEl);
   }
+
+  // add a heading for tracking totals to the end of the row
+  var totalsCell = document.createElement('th');
+  totalsCell.textContent = 'Total Cookies';
+  headerRow.appendChild(totalsCell);
 };
 
+// renders the body row for the table
+StoreLocation.prototype.renderBodyRow = function() {
+
+  // store table element in variable
+  var tableEl = document.getElementById('cookieSalesTable');
+
+  // tr1 - create table row element for table
+  var bodyRow = document.createElement('tr');
+
+  // append a row heading to the start of the row
+  var rowHeading = document.createElement('th');
+  rowHeading.textContent = this.name;
+  bodyRow.appendChild(rowHeading);
+
+  // tr2 - append table row element to table element
+  tableEl.appendChild(bodyRow);
+
+  // loop through the cookies sold array to log each hour's cookie sales
+  for(var i = 0; i < this.cookiesSoldArray.length; i++) {
+    // th1 - create table data element
+    var headingEl = document.createElement('td');
+    // th2 - insert the data
+    headingEl.textContent = this.cookiesSoldArray[i];
+    // th3 - append to the DOM
+    bodyRow.appendChild(headingEl);
+  }
+
+  // add a heading for tracking totals to the end
+  var totalsCell = document.createElement('td');
+  totalsCell.textContent = this.cookiesToday;
+  bodyRow.appendChild(totalsCell);
+};
+
+// renders the header and the body
+var renderTable = function(storeLocationArray) {
+  
+  // build the heading row
+  renderHeaderRow();
+
+  // loop through the given array and activate each object's
+  // calculate and render methods
+  for(var i = 0; i < storeLocationArray.length; i++) {
+    // calculate the cookies so the object has data
+    storeLocationArray[i].calculateDailyCookies();
+    // use the object's data to render the table's body rows
+    storeLocationArray[i].renderBodyRow();
+  }
+};
+
+// create all StoreLocation objects
 var firstAndPike = new StoreLocation('First and Pike', 23, 65, 6.3);
 var seaTacAirport = new StoreLocation('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new StoreLocation('Seattle Center', 11, 38, 3.7);
 var capitolHill = new StoreLocation('Capitol Hill', 20, 38, 2.3);
 var alki = new StoreLocation('Alki', 2, 16, 4.6);
 
-console.log(firstAndPike);
-console.log(seaTacAirport);
-console.log(seattleCenter);
-console.log(capitolHill);
-console.log(alki);
+// store all StoreLocation objects in an array for looping in renderTable function
+var storeLocationArray = [
+  firstAndPike,
+  seaTacAirport,
+  seattleCenter,
+  capitolHill,
+  alki,
+];
 
-firstAndPike.calculateDailyCookies();
-firstAndPike.renderHeaderRow();
+// render a table to include all the objects in the array
+renderTable(storeLocationArray);
